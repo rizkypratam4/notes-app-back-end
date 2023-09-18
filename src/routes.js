c4bdbfb1f297942bcddd2907a7594a528ede4777
RizkyPratama <rizkypratamalvbis@gmail.com>
2023-09-18 17:09:16 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+
+  deleteNoteByHandler,
+  editNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByHandler,
+  },
+];
+
+module.exports = routes;
